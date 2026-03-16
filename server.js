fe184018f41c5b32e3d18344ff8a6ff9f05e83fd
mamad41 <mamadou.baradji@campus-centre.fr>
2026-03-16 15:06:39 +0100

    maj

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
 
 const db = require("./db");
 
